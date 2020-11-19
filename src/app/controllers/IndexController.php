d19c75f71ad75a5f6b5fe98348fa4321901af46a
Beon dika <dika@jagoanhosting.com>
2020-11-20 06:44:08 +0700

    first commit

---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MyApp\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo '[' . __METHOD__ . ']';
+    }
+
+    
+}
---
+++ src/app/controllers/RoomController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MyApp\Controllers;
+
+class RoomController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo '[' . __METHOD__ . ']';
+    }
+
+    
+}
